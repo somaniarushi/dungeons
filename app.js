eb630c49754e0c2c4291760f8963b73dfef8b66f
Arushi Somani <arushisomani@berkeley.edu>
2021-06-14 19:27:04 -0700

    a flaccid first attempt

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var http = require('http');
+
+var app = express();
+var server = http.createServer(app);
+
+var io = require('socket.io')(server);
+var path = require('path');
+
+
+app.use(express.static(path.join(__dirname,'./public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+
+var name;
+
+io.on('connection', (socket) => {
+  console.log('new user connected');
+  
+  socket.on('joining msg', (username) => {
+  	name = username;
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('change', (loc) => {
+    socket.broadcast.emit('change', loc);         
+    //sending message to all except the sender
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('Server listening on :3000');
+});
+
+
